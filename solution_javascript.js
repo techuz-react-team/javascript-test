f5c91050af5cd54478928293b0fc15c387e76822
Vimpy Jain <vimpy@techuz.com>
2021-01-25 14:44:40 +0530

    solution of test

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,19 +1,51 @@
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this.allOpearations = [];
+    this.undoOperations = [];
+    this.currentTipIndex = 0;
   }
 
-  add(num) {}
+  add(num) {
+    this.value += num;
+    this.allOpearations.push(num);
+    this.currentTipIndex += 1;
+  }
 
-  subtract(num) {}
+  subtract(num) {
+    this.value -= num;
+    this.allOpearations.push(-num);
+    this.currentTipIndex += 1;
+  }
 
-  undo() {}
+  undo() {
+    this.currentTipIndex = this.currentTipIndex - 1;
+    if (this.allOpearations[this.currentTipIndex]) {
+      this.value = this.value - this.allOpearations[this.currentTipIndex];
+    } else {
+      this.value = 0;
+    }
+    this.undoOperations.push(this.allOpearations[this.currentTipIndex]);
+  }
 
-  redo() {}
+  redo() {
+    let redoOperation = this.undoOperations.pop();
+    if (redoOperation) {
+      this.value = this.value + redoOperation;
+    }
+  }
 
-  bulk_undo(num) {}
+  bulk_undo(num) {
+    for(let i=0;i<num;i++) {
+      this.undo();
+    }
+  }
 
-  bulk_redo(num) {}
+  bulk_redo(num) {
+    for(let i=0;i<num;i++) {
+      this.redo();
+    }
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
