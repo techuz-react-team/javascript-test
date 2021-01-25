cf46537ebaebb52849ebd0cb0e13693ceb2be55d
khushboo <khushbooj@techuz.com>
2021-01-25 15:20:13 +0530

    test code committed

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,19 +1,49 @@
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this.AllAction = [];
+    this.undoStep = 0;  // test undo no actions
+    this.redoStep = 0;  // test redo no actions
   }
 
-  add(num) {}
+  add(number) {
+    this.value += number;
+    this.undoStep = number;
+    this.AllAction.push(number);
+  }
 
-  subtract(num) {}
+  subtract(number) {
+    this.value -= number;
+    this.redoStep = number;
+    this.AllAction.push(number);
+  }
 
-  undo() {}
+  undo() {
+    this.subtract(this.undoStep);
+  }
 
-  redo() {}
+  redo() {
+    this.add(this.redoStep);
+  }
 
-  bulk_undo(num) {}
+  bulk_undo(number) {
+    this.value = 0;
+    // this.undoArray.map(number => {
+    //   this.undoArray.pop();
+    // });
+    for (let i = 0; i < number; i++) {
+      this.AllAction.pop();
+    }
+    for (let val of this.AllAction) {
+      this.value += val;
+    }
+  }
 
-  bulk_redo(num) {}
+  bulk_redo(number) {
+    for (let i = 0; i < number; i++) {
+      this.value += this.AllAction.pop();
+    }
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
